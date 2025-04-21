9915a0f6f122dda465a2713c0129380f0b98b56a
Dzung <dzung2509@Dzungs-MacBook-Pro.local>
2025-04-22 00:13:03 +0700

    feat: fix commit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -13,25 +13,9 @@ export interface IButton {
   onPress?: (e: GestureResponderEvent) => void;
   type?: 'primary' | 'secondary';
 }
-export const Button = ({
-  onPress,
-  title,
-  buttonStyles,
-  textBtnStyles,
-}: IButton) => {
+export const Button = ({ onPress, title, textBtnStyles }: IButton) => {
   return (
-    <TouchableOpacity
-      style={{
-        // borderWidth: theme?.button_borderWidth ?? 0,
-        // borderColor: theme?.button_borderColor ?? 'white',
-        // width: theme?.button_width ?? 50,
-        // height: theme?.button_height ?? 60,
-        backgroundColor: 'red',
-        // padding: theme?.button_padding ?? 8,
-        ...buttonStyles,
-      }}
-      onPress={onPress}
-    >
+    <TouchableOpacity onPress={onPress}>
       <Text
         style={{
           // color: typography?.['Text_text-primary_(700)'] ?? 'white',
