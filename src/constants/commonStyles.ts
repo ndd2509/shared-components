92939e1f5acc4231e6741e4a5375c28b340751c6
DungND-paypay <dungnguyendinh@paypay.com.vn>
2026-02-06 14:55:17 +0700

    fix: update folder

--- src/constants/commonStyles.ts
+++ src/constants/commonStyles.ts
@@ -1,11 +1,4 @@
 import { Dimensions } from 'react-native';
-import type { TxtLineHeight, TxtSize, TypeWeightTxt } from '../types/types';
-
-interface IStylesTxt {
-  s?: TxtSize;
-  lh?: TxtLineHeight;
-  w?: TypeWeightTxt;
-}
 
 export const SCREEN_WIDTH: number = Dimensions.get('window').width;
 export const SCREEN_HEIGHT: number = Dimensions.get('window').height;
@@ -20,16 +13,5 @@ export const space_squish = ({
   return {
     paddingVertical: vertical,
     paddingHorizontal: horizontal,
-  };
-};
-
-export const styleTypography = (props: IStylesTxt) => {
-  //s la fontsize, lh la lineHeigh, w: fontWeight
-  const { s = 18, lh = 28, w = 'regular' } = props;
-
-  return {
-    fontSize: s,
-    lineHeight: lh,
-    fontWeight: w,
   };
 };
--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,3 @@
 export * from './components';
 export * from './constants';
 export * from './utils';
-export * from './types';
